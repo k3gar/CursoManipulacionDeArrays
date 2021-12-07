3177317466d04d614af1edf3360f6a339f8d3a59
k3gar <kevinede40@gmail.com>
2021-12-07 15:25:02 -0600

    02 Map

---
+++ server/02-map.js
@@ -0,0 +1,11 @@
+const letters = ['a', 'b', 'c', 'd'];
+const newArray = letters.map(item => item + '**')
+
+/* const newArray = [];
+
+for (let index = 0; index < letters.length; index++) {
+    const element = letters[index];
+    newArray.push(element + '++');
+} */
+console.log('Original', letters)
+console.log('Array Nuevo', newArray)
